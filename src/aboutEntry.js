8525579bf960a8739aed4a46e5014f1b1e683702
Luke <694014820@qq.com>
2020-06-12 13:10:46 +0800

    m

--- src/aboutEntry.js
+++ src/aboutEntry.js
@@ -1,6 +1,5 @@
 /* weex initialized here, please do not move this line */
-const { router } = require('./router');
 const App = require('@/about.vue');
 /* eslint-disable no-new */
-new Vue(Vue.util.extend({el: '#root', router}, App));
+new Vue(Vue.util.extend({el: '#root'}, App));
 //router.push('/boot');
