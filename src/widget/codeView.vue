b7dbbf91cfca98fc8016ee2cf7c7d8b9734c4c00
Luke <694014820@qq.com>
2020-06-29 22:09:17 +0800

    m

--- src/widget/codeView.vue
+++ src/widget/codeView.vue
@@ -16,7 +16,8 @@
                 <text :class="text.class"
                       v-for="(text) in line">{{text.text||' '}}</text>
             </cell>
-            <cell style="height: 0.5px;background-color: #888888">
+            <cell v-if="displayLines.length>0"
+                  style="height: 0.5px;background-color: #888888">
             </cell>
         </list>
     </scroller>
@@ -26,7 +27,7 @@
 <script>
     import hljs from 'highlight.js'
     import {Utils} from 'weex-ui'
-    import {Element, CharacterData} from '../libs/domino/impl'
+    import {Element} from '../libs/domino/impl'
     import domino from '@/libs/domino/index'
     import utils from "@/libs/utils";
 
@@ -40,7 +41,7 @@
         },
         methods: {
             getLineText(current) {
-                let max = this.allLines.lines.length
+                let max = this.allLines.lines.length;
 
                 function getLength(i) {
                     let l = 0;
@@ -51,12 +52,12 @@
                     return l
                 }
 
-                let space = getLength(max) - getLength(current)
-                let s = []
+                let space = getLength(max) - getLength(current);
+                let s = [];
                 for (let i = 0; i < space; i++) {
                     s.push(null)
                 }
-                let text = current.toString()
+                let text = current.toString();
                 for (let i = 0; i < text.length; i++) {
                     s.push(text[i])
                 }
@@ -74,88 +75,53 @@
                 return Utils.env.getScreenHeight()
             },
             allLines() {
-                function highlightLines(language, codeText) {
+                function build(language, source) {
 
                     function getClassList(node) {
-                        let classList = ['text-span']
+                        let classList = ['text-span'];
                         for (let j = 0; j < node.classList.length; j++) {
-                            let c = node.classList[j]
+                            let c = node.classList[j];
                             classList.push(c)
                         }
                         return classList
                     }
 
                     function appendSpan(env, text, classList) {
-                        if (classList) {
-                            env.line.push({
-                                class: classList,
-                                text: text
-                            })
-                        } else {
-                            env.line.push({
-                                class: ['text-span'],
-                                text: text
-                            })
-                        }
+                        env.line.push({
+                            class: classList,
+                            text: text
+                        });
                         env.width += text.length
                     }
 
-                    function buildLine(env, node, classList) {
-                        for (let i = 0; i < node.childNodes.length; i++) {
-                            let item = node.childNodes[i]
-                            if (item instanceof CharacterData) {
-                                if (item.data.length > 0) {
-                                    if (item.data.indexOf('\n') !== -1) {
-                                        let texts = item.data.split('\n')
-                                        for (let j = 0; j < texts.length; j++) {
-                                            let span = texts[j]
-                                            if (span.length > 0) {
-                                                appendSpan(env, span, classList)
-
-                                            }
-                                            if (j < texts.length - 1) {
-                                                env.lines.push(env.line)
-                                                env.maxWidth = Math.max(env.maxWidth, env.width)
-                                                env.width = 0
-                                                env.line = []
-                                            }
-                                        }
-                                    } else {
-                                        appendSpan(env, item.data, classList)
-                                    }
+                    function build2(env, node) {
+                        let childNodes = node.childNodes;
+                        let classList = getClassList(node);
+                        for (let i = 0; i < childNodes.length; i++) {
+                            let node = childNodes[i];
+                            if (node instanceof Element) {
+                                if (node.tagName === 'BR') {
+                                    env.lines.push(env.line);
+                                    env.maxWidth = Math.max(env.maxWidth, env.width);
+                                    env.width = 0;
+                                    env.line = []
+                                } else {
+                                    build2(env, node)
                                 }
-                            } else if (item instanceof Element) {
-                                if (item.tagName === 'SPAN') {
-                                    let classList2 = getClassList(item)
-                                    if (classList && classList2.length === 1) {
-                                        for (let j = 0; j < classList.length; j++) {
-                                            let c = classList[j]
-                                            if (classList2.indexOf(c) === -1) {
-                                                classList2.push(c)
-                                            }
+                            } else {
+                                let textContent = node.textContent;
+                                if (textContent.length > 0) {
+                                    let textSpans = textContent.split('\n');
+                                    for (let j = 0; j < textSpans.length; j++) {
+                                        let span = textSpans[j];
+                                        if (span.length > 0) {
+                                            appendSpan(env, span, classList)
                                         }
-                                    }
-                                    if (item.children.length > 0) {
-                                        buildLine(env, item, classList2)
-                                    } else {
-                                        if (item.textContent.length > 0) {
-                                            if (item.textContent.indexOf('\n') !== -1) {
-                                                let texts = item.textContent.split('\n')
-                                                for (let j = 0; j < texts.length; j++) {
-                                                    let span = texts[j]
-                                                    if (span.length > 0) {
-                                                        appendSpan(env, span, classList2)
-                                                    }
-                                                    if (j < texts.length - 1) {
-                                                        env.lines.push(env.line)
-                                                        env.maxWidth = Math.max(env.maxWidth, env.width)
-                                                        env.width = 0
-                                                        env.line = []
-                                                    }
-                                                }
-                                            } else {
-                                                appendSpan(env, item.textContent, classList2)
-                                            }
+                                        if (j < textSpans.length - 1) {
+                                            env.lines.push(env.line);
+                                            env.maxWidth = Math.max(env.maxWidth, env.width);
+                                            env.width = 0;
+                                            env.line = []
                                         }
                                     }
                                 }
@@ -163,33 +129,37 @@
                         }
                     }
 
-                    language = language || 'txt'
-                    let highlight;
-                    if (language && hljs.getLanguage(language)) {
-                        highlight = hljs.highlight(language, codeText).value
-                    } else {
-                        highlight = hljs.highlightAuto(codeText).value
-                    }
-                    let html = "<html><head><title></title></head><body>" + highlight + "<br></body></html>"
-                    utils.copy(html)
-                    let root = domino.createDocument(html).body
                     let env = {
                         lines: [],
                         line: [],
                         maxWidth: 0,
                         width: 0
+                    };
+
+                    language = language || 'txt';
+                    let highlight;
+                    if (language && hljs.getLanguage(language)) {
+                        highlight = hljs.highlight(language, source).value
+                    } else {
+                        highlight = hljs.highlightAuto(source).value
                     }
-                    buildLine(env, root)
+                    let html = "<html><head><title></title></head><body>" + highlight + "<br></body></html>";
+                    utils.copy(html)
+
+                    let root = domino.createDocument(html).body;
+
+                    build2(env, root);
+
                     return {
                         lines: env.lines,
                         maxWidth: env.maxWidth
                     }
                 }
 
-                return highlightLines(this.language, this.codeText)
+                return build(this.language, this.codeText);
             },
             displayLines() {
-                let lines = this.allLines.lines
+                let lines = this.allLines.lines;
                 return lines.slice(0, Math.min(this.page * 50, lines.length))
             }
         },
