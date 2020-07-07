9345f7ce5c1afc096058befbc02c3cf6f30bdbf8
Luke <694014820@qq.com>
2020-07-08 04:50:48 +0800

    m

---
+++ src/pages/pr/index.js
@@ -0,0 +1,5 @@
+/* weex initialized here, please do not move this line */
+const App = require('@/pages/pr/index.vue');
+/* eslint-disable no-new */
+new Vue(Vue.util.extend({el: '#root'}, App));
+//router.push('/boot');
